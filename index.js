65ca3b2248f1b8ae10555443c49487f204cd25a8
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-16 16:25:00 -0400

    Cors update

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   process.env.DB_CONNECTION,
