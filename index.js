5194e6ea3d53991fa3193c2e0abbd4791c2405de
NateTheDev1 <>
2020-06-17 18:09:37 -0400

    BodyParser Update

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 
 dotenv.config();
 app.use(cors())
-app.use(express.json());
+app.use(bodyParser.json());
 
 mongoose.connect(
   process.env.DB_CONNECTION,
